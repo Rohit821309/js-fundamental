d05bd901b7bfa065bcd9a44c31fec32328823068
Rohit821309 <rohittiwari821309@gmail.com>
2026-07-12 21:32:43 +0530

    Added data types examples

---
+++ data_types.js
@@ -0,0 +1,15 @@
+"use strict" //treat all js code as newer version
+// alert(3+4) // we are using nodejs not browser so it will give error
+// code readability should be high
+// some data types in js
+let name = "Rohit"; //string
+let age = 2; //number
+// bigint
+let bigInt = 1234567890123456789012345678901234567890n;
+let isLoggedIn = true; //boolean
+let accountNo; //undefined
+let accountBalance = null; //null
+// NOTE:- Null is an object in js but it is used to represent empty or non-existent value
+// undefined is a type itself and it is used to represent uninitialized variable    
+console.log(typeof accountNo);
+// typeof operator is used to find the data type of a variable
